844f66abc2a8d317428112985e04c94960c184d8
kashipu <graficowm@gmail.com>
2021-10-27 20:26:00 -0500

    Añadido parcial final, Javascrip menú hamburguesa

---
+++ parcialFinal/js/main.js
@@ -0,0 +1,9 @@
+let burguerButton = document.getElementById('buttonBurguer');
+let navList = document.getElementById('navList');
+let navButton = document.getElementById('navButton');
+
+function toggleMenu() {
+    navList.classList.toggle('show');
+    navButton.classList.toggle('show');
+}
+
